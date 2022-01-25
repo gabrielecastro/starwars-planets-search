8bf142078452e571edf31057761fedf1b6d5db30
Gabriele Castro <gabrielerayane2001@gmail.com>
2022-01-25 15:32:27 -0300

    Adiciona o Provider no App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import AppProvider from './context/AppProvider';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <AppProvider>
+      <span>Hello, App!</span>
+    </AppProvider>
   );
 }
 
